1ee853c44a4793a5eb1d1a2de4e518bff6a30ecf
VictorKaue <98410524+VictorKaue@users.noreply.github.com>
2024-11-19 09:38:41 -0300

    Add files via upload

---
+++ Gerenciamento_Estudantil/src/interface_grafica/Estudante.java
@@ -0,0 +1,15 @@
+package interface_grafica;
+
+public class Estudante extends Pessoa {
+	private int matricula;
+
+	public Estudante(String nome, int idade, int matricula) {
+		super(nome, idade);
+		this.matricula = matricula;
+	}
+	
+	public void exibirDados() {
+		
+	}
+	
+}
