bb89d6569a8738424c7c62420768b0850f72162d
VictorKaue <98410524+VictorKaue@users.noreply.github.com>
2024-11-25 23:34:33 -0300

    Update CreateDatabase.sql

--- CreateDatabase.sql
+++ CreateDatabase.sql
@@ -1,3 +1,4 @@
+-- Criar a tabela Professor
 CREATE TABLE Professor (
     ProfessorID VARCHAR(50) PRIMARY KEY,  -- A chave primária será do tipo STRING (VARCHAR)
     Nome VARCHAR(100) NOT NULL,  -- Nome do Professor
@@ -9,7 +10,7 @@ CREATE TABLE Professor (
 CREATE TABLE Curso (
     CursoID INT AUTO_INCREMENT PRIMARY KEY,  -- A chave primária do curso continua como inteiro
     Nome VARCHAR(100) NOT NULL,  -- Nome do Curso
-    CargaHoraria INT, -- Carga Horário do Curso
+    CargaHoraria INT, -- Carga Horária do Curso
     ProfessorID VARCHAR(50),  -- Chave estrangeira para associar o Curso ao Professor
     CONSTRAINT FK_ProfessorCurso FOREIGN KEY (ProfessorID)  -- Relacionamento entre Curso e Professor
     REFERENCES Professor(ProfessorID) ON DELETE SET NULL
@@ -19,8 +20,16 @@ CREATE TABLE Curso (
 CREATE TABLE Aluno (
     AlunoID VARCHAR(50) PRIMARY KEY,  -- A chave primária será do tipo STRING (VARCHAR)
     Nome VARCHAR(100) NOT NULL,   -- Nome do Aluno
-    Idade INT, -- Idade do aluno
-    CursoID INT,  -- Chave estrangeira para associar o Aluno ao Curso
-    CONSTRAINT FK_CursoAluno FOREIGN KEY (CursoID)  -- Relacionamento entre Aluno e Curso
-    REFERENCES Curso(CursoID) ON DELETE SET NULL
+    Idade INT   -- Idade do aluno
+);
+
+-- Criar a tabela associativa Aluno_Curso para o relacionamento N para N entre Aluno e Curso
+CREATE TABLE Aluno_Curso (
+    AlunoID VARCHAR(50),   -- Chave estrangeira para a tabela Aluno
+    CursoID INT,           -- Chave estrangeira para a tabela Curso
+    PRIMARY KEY (AlunoID, CursoID),  -- A combinação de AlunoID e CursoID deve ser única
+    CONSTRAINT FK_Aluno FOREIGN KEY (AlunoID)  -- Relacionamento entre Aluno e Aluno_Curso
+    REFERENCES Aluno(AlunoID) ON DELETE CASCADE,
+    CONSTRAINT FK_Curso FOREIGN KEY (CursoID)  -- Relacionamento entre Curso e Aluno_Curso
+    REFERENCES Curso(CursoID) ON DELETE CASCADE
 );
